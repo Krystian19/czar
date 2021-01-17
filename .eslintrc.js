cac2b885342df8250d8fb4fb4b4ce1f9227ad669
Jan Guzman <janfrancisco19@gmail.com>
2021-01-17 23:23:05 +0100

    Removed unnecessary react dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     '@typescript-eslint'
   ],
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
+    "airbnb-typescript/base",
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -16,15 +15,12 @@ module.exports = {
     "no-restricted-syntax": 0,
     'no-underscore-dangle': 0,
     'class-methods-use-this': 0,
-    'jsx-a11y/anchor-is-valid': 0,
     'lines-between-class-members': 0,
     '@typescript-eslint/camelcase': 0,
     'max-len': ["error", { "code": 88 }],
     '@typescript-eslint/no-this-alias': 0,
     '@typescript-eslint/no-var-requires': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
     '@typescript-eslint/no-empty-interface': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
     "@typescript-eslint/no-unused-vars": ["error"],
     "@typescript-eslint/explicit-function-return-type": ["error"],
     "import/order": ["error", {
