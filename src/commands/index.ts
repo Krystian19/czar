e8e7e3d935ebff085872eb83ac804d2680676d76
Jan Guzman <janfrancisco19@gmail.com>
2021-02-07 17:01:27 +0100

    feat: author command (#16)
    * feat: author command
    
    * connect author command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import { Command } from '../common';
 import PlayCommand from './play';
 import StopCommand from './stop';
+import AuthorCommand from './author';
 
 const cmds: Command[] = [
   PlayCommand,
   StopCommand,
+  AuthorCommand,
 ];
 
 export default cmds;
