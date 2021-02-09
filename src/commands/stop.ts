154e9cfee0fae435e41d8c953e7eb2b984b9e529
Jan Guzman <janfrancisco19@gmail.com>
2021-02-09 16:45:05 +0100

    fix: change status properly when music stops (#21)

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,11 +1,19 @@
-import { Command } from '../common';
+import { Command, setClientActivity } from '../common';
 
 const stopCMD: Command = {
   name: 'stop',
   description: 'Stop',
-  execute: async (msg): Promise<void> => {
+  execute: async (msg, client): Promise<void> => {
     const { channel } = msg.member.voice;
     channel.leave();
+
+    try {
+      await setClientActivity(client);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+
+    return Promise.resolve();
   },
 };
 
